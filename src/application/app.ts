9cff37aca5ae990c2794372bdafa8544c54d8c24
souiyaaa <153163259+souiyaaa@users.noreply.github.com>
2025-01-02 20:04:52 +0700

    App.ts improved with authentication
    Menyala Jollyne

--- src/application/app.ts
+++ src/application/app.ts
@@ -2,12 +2,18 @@ import express from "express"
 import { publicRouter } from "../routes/public-router"
 import { errorMiddleware } from "../middleware/error-middleware"
 import { protectedRouter } from "../routes/protected-router"
+import { authMiddleware } from "../middleware/auth-middleware"
 
 const app = express()
 app.use(express.json())
 
+// Public routes do not require authentication
 app.use(publicRouter)
+
+// Protected routes need authentication
+app.use(authMiddleware, protectedRouter)
+
+// Error handling middleware should be last
 app.use(errorMiddleware)
-app.use(protectedRouter)
 
 export default app
